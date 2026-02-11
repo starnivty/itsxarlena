f6dae6a5b7e34f36b43b4cb5cd31c4fee1be1703
Aleyna <starnivty@example.com>
2026-02-12 06:48:20 +0700

    delete slug

--- app/api/test/[slug]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET(
-  req: Request,
-  { params }: { params: { slug: string } }
-) {
-  return NextResponse.json({ slug: params.slug })
-}
